3b4fbcc3db528e75ce69f5084e22f8b996d46ac3
vishalcreatiosoft <vishal.srivastava@creatiosoft.com>
2022-03-08 13:24:24 +0530

    redis connected

--- controller/employee-controller.js
+++ controller/employee-controller.js
@@ -1,12 +1,22 @@
 const Employee = require('../models/employee');
+const redis = require('redis');
 
 const employees = {};
+const client = redis.createClient();
+
+client.connect();
+
+client.on('connect', () => {
+    console.log('connected..');
+})
+
 
 employees.saveData = async(data) => {
 
     try {
         const newEmployee = new Employee(data);
         const saveEmployee = newEmployee.save();
+        const registerdName = await client.set('username', `${data.firstName} ${data.lastName}`);
         return true
 
     } catch (err) {
